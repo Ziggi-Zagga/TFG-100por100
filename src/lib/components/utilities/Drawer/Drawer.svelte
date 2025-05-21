94f49016da4595411274ada220ba5696788988a1
Ziggi-Zagga <danielruizmarti752@gmail.com>
2025-05-21 21:08:05 +0200

    fix: upgrade drawer

--- src/lib/components/utilities/Drawer/Drawer.svelte
+++ src/lib/components/utilities/Drawer/Drawer.svelte
@@ -1,7 +1,8 @@
-<script lang="ts">
+<script lang="ts"> 
 	import { fly } from 'svelte/transition';
 	export let title: string;
 	export let onClose: () => void;
+	export let children: () => void;
 
 	function handleKeyClose(event: KeyboardEvent) {
 		if (event.key === 'Escape') {
@@ -22,10 +23,15 @@
 
 <!-- Drawer Panel -->
 <div
-	class="fixed top-0 right-0 z-50 h-full w-30% max-w-3xl overflow-y-auto rounded-l-3xl border-l border-blue-100 bg-white shadow-2xl p-10"
+	class="fixed top-0 right-0 z-50 h-full w-full max-w-3xl overflow-y-auto rounded-l-3xl border-l border-blue-100 bg-white shadow-2xl p-10"
 	in:fly={{ x: 300, duration: 300 }}
 	out:fly={{ x: 300, duration: 200 }}
 >
+	<!-- Título del Drawer -->
 	<h2 class="mb-8 text-3xl font-bold text-blue-800">{title}</h2>
-	<slot />
+
+	<!-- Contenido del formulario u otros elementos -->
+	<div class="space-y-4">
+		{@render children?.()}
+	</div>
 </div>
